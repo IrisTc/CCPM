4644aaec8104ad8906819beefb932edfc0dba739
IrisTc <1010939908@qq.com>
2020-12-21 21:54:38 +0800

    完成邀请成员

--- app/src/main/java/com/iris/ccpm/MemberSearchActivity.java
+++ app/src/main/java/com/iris/ccpm/MemberSearchActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,18 +19,25 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.iris.ccpm.model.Member;
+import com.iris.ccpm.model.Project;
 import com.iris.ccpm.utils.NetCallBack;
 import com.iris.ccpm.utils.Request;
+
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class MemberSearchActivity extends AppCompatActivity {
     EditText etSearch;
     TextView btSearch;
     LinearLayout ly;
+    String project_id;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_search);
+
+        Intent intent = this.getIntent();
+        project_id = intent.getStringExtra("project_id");
 
         etSearch = findViewById(R.id.et_search);
         btSearch = findViewById(R.id.bt_search);
@@ -47,16 +55,17 @@ public class MemberSearchActivity extends AppCompatActivity {
                         TextView tvEmail = findViewById(R.id.member_email);
                         TextView tvPosition = findViewById(R.id.member_position);
                         SimpleDraweeView ivAvatar = findViewById(R.id.member_avatar);
-                        ImageView btShow = findViewById(R.id.member_personal_button);
+                        ImageButton btShow = findViewById(R.id.member_personal_button);
 
                         Member member = JSON.toJavaObject(result, Member.class);
 
                         tvName.setText(member.getName());
-                        tvEmail.setText(member.getPhoneNum());
+                        tvEmail.setText(member.getUsername());
                         tvPosition.setText(member.getPosition());
                         Uri uri = Uri.parse(member.getAvatarUrl());
                         ivAvatar.setImageURI(uri);
 
+                        ly.setVisibility(View.VISIBLE);
                         btShow.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
@@ -67,13 +76,31 @@ public class MemberSearchActivity extends AppCompatActivity {
                                 builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
+                                        JSONObject body = new JSONObject();
+                                        StringEntity entity = new StringEntity(body.toJSONString(), "UTF-8");
+                                        Request.clientPost(MemberSearchActivity.this, "project/" + project_id + "/invite/" + member.getUsername(), entity, new NetCallBack() {
+                                            @Override
+                                            public void onMySuccess(JSONObject result) {
+                                                System.out.println(result);
+                                                Toast.makeText(MemberSearchActivity.this, "邀请成功", Toast.LENGTH_LONG).show();
+                                            }
 
+                                            @Override
+                                            public void onMyFailure(String error) {
+                                                System.out.println(error);
+                                                Toast.makeText(MemberSearchActivity.this, error, Toast.LENGTH_LONG).show();
+                                            }
+                                        });
                                     }
                                 });
+                                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                    }
+                                });
+                                builder.show();
                             }
                         });
-
-                        ly.setVisibility(View.VISIBLE);
                     }
 
                     @Override
