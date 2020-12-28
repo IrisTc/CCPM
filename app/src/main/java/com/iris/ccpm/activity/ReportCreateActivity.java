e82b53163a2e14abf833aeb4a916092db1089115
IrisTc <1010939908@qq.com>
2020-12-28 19:14:24 +0800

    完成安卓 拍摄照片功能

--- app/src/main/java/com/iris/ccpm/activity/ReportCreateActivity.java
+++ app/src/main/java/com/iris/ccpm/activity/ReportCreateActivity.java
@@ -1,18 +1,35 @@
 package com.iris.ccpm.activity;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.alibaba.fastjson.JSONObject;
 import com.iris.ccpm.R;
@@ -20,6 +37,12 @@ import com.iris.ccpm.adapter.TaskDetailSpinnerAdapter;
 import com.iris.ccpm.model.TaskModel;
 import com.iris.ccpm.utils.NetCallBack;
 import com.iris.ccpm.utils.Request;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import cz.msebera.android.httpclient.entity.StringEntity;
 
@@ -38,6 +61,11 @@ public class ReportCreateActivity extends AppCompatActivity {
     EditText etContent;
     EditText etPreHour;
     Button btReport;
+    ImageButton ivCamera;
+    ImageView ivImage;
+    private File file;
+    private Uri imageUri;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 999;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,6 +78,26 @@ public class ReportCreateActivity extends AppCompatActivity {
         findView();
         initSpinner();
         loadData();
+
+        initPhotoError();
+        ivCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("camera");
+                //第二个参数是需要申请的权限
+                if (ContextCompat.checkSelfPermission(ReportCreateActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(ReportCreateActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    //权限还没有授予
+                    ActivityCompat.requestPermissions(ReportCreateActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                } else {
+                    //权限已经被授予
+                    takePhoto();
+                }
+            }
+        });
 
         btReport.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +127,74 @@ public class ReportCreateActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_PHONE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                takePhoto();
+            } else {
+                // Permission Denied
+                Toast.makeText(ReportCreateActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println(imageUri);
+
+        //0 拍照
+            try {
+                System.out.println("uri" + getContentResolver().openInputStream(imageUri));
+                Bitmap bit = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                ivImage.setImageBitmap(bit);
+            } catch (Exception e) {
+                System.out.println(e);
+                Toast.makeText(this, "程序崩溃", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    void takePhoto() {
+        System.out.println(Environment.getExternalStorageDirectory().getAbsolutePath());
+        File folder = new File(Environment.getExternalStorageDirectory().getAbsolutePath(),"");
+        if(!folder.exists()){
+            folder.mkdir();
+        }
+
+        /**
+         * 这里将时间作为不同照片的名称
+         */
+        file = new File(folder, "cache.jpg");
+
+        /**
+         * 如果该文件已经存在，则删除它，否则创建一个
+         */
+        try {
+            if (file.exists()) {
+                file.delete();
+            }
+            file.createNewFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        /**
+         * 隐式打开拍照的Activity，并且传入CROP_PHOTO常量作为拍照结束后回调的标志
+         */
+        imageUri = Uri.fromFile(file);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, 2);
+    }
+
+    private void initPhotoError() {
+        // android 7.0系统解决拍照的问题
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        builder.detectFileUriExposure();
     }
 
     private void loadData() {
@@ -114,6 +230,8 @@ public class ReportCreateActivity extends AppCompatActivity {
         etPreHour = findViewById(R.id.et_prehour);
         btReport = findViewById(R.id.bt_report);
         completeSpinner = findViewById(R.id.completeSpinner);
+        ivCamera = (ImageButton) findViewById(R.id.iv_camera);
+        ivImage = findViewById(R.id.iv_image);
     }
 
     private void initSpinner() {
--- app/src/main/java/com/iris/ccpm/activity/taskCreate/TaskCreateActivity.java
+++ app/src/main/java/com/iris/ccpm/activity/taskCreate/TaskCreateActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -13,6 +14,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
